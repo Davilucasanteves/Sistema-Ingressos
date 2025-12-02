6f885c123f1c1fc4b58602390ed6896496ec3599
Lívia Maria <liviamariaprates236@gmail.com>
2025-12-02 07:56:49 -0300

    Atualização de telas, inserção das que faltaram e dos seus controllers

---
+++ src/controller/TelaApagarFestaController.java
@@ -0,0 +1,37 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class TelaApagarFestaController {
+
+    @FXML
+    private AnchorPane anchorPaneCadastraFesta;
+
+    @FXML
+    private Button buttomVoltar;
+
+    @FXML
+    private Button buttonApagarFesta;
+
+    @FXML
+    private Label labelStatusCadastro;
+
+    @FXML
+    private TextField textFieldIdFesta;
+
+    @FXML
+    void handleButtonApagarFesta(ActionEvent event) {
+
+    }
+
+    @FXML
+    void voltarAdm(ActionEvent event) {
+
+    }
+
+}
---
+++ src/controller/TelaEditarFestaController.java
@@ -0,0 +1,46 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class TelaEditarFestaController {
+
+    @FXML
+    private AnchorPane anchorPaneCadastraFesta;
+
+    @FXML
+    private Button buttomVoltar;
+
+    @FXML
+    private Button buttonSalvarFesta;
+
+    @FXML
+    private Label labelStatusCadastro;
+
+    @FXML
+    private TextField textFieldData;
+
+    @FXML
+    private TextField textFieldDescricao;
+
+    @FXML
+    private TextField textFieldIdFesta;
+
+    @FXML
+    private TextField textFieldNomeFesta;
+
+    @FXML
+    void handleButtonSalvarFesta(ActionEvent event) {
+
+    }
+
+    @FXML
+    void voltarAdm(ActionEvent event) {
+
+    }
+
+}
